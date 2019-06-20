966962b4c5dcca9eec6b2afccd00e3f240977d76
dmytzo <dmitriytris@gmail.com>
2019-06-21 02:08:54 +0300

    Add middlaware

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from api import API
+from middleware import Middleware
 
 app = API()
+
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = "Oops! Something went wrong."
+
+app.add_exception_handler(custom_exception_handler)
+
+
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, res):
+        print("Processing response", req.url)
+
+app.add_middleware(SimpleCustomMiddleware)
+
+
+@app.route("/exc")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be user")
+
 
 @app.route("/home")
 def home(request, response):
